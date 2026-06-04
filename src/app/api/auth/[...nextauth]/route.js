1ce0671284775037508769ba05e9ea7e353174cd
AnthonyLoche <anthonylocheifc@gmail.com>
2026-06-04 00:17:36 -0300

    FEAT: fix

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,6 @@
-export async function GET() {
-  return Response.json({
-    ok: true,
-    message: "Funcionando"
-  });
-}
+import NextAuth from "next-auth";
+import { authOptions } from "@/modules/auth/auth";
 
-export async function POST() {
-  return Response.json({
-    ok: true
-  });
-}
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
